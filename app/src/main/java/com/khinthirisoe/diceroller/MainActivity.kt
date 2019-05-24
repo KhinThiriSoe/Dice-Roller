ee3f5d531ca1a9acf087444e4bc37756fcc7e5b8
Khin <khinthirisoe.mdy@gmail.com>
2019-05-24 14:21:04 +0700

    add rolldice button

--- app/src/main/java/com/khinthirisoe/diceroller/MainActivity.kt
+++ app/src/main/java/com/khinthirisoe/diceroller/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.khinthirisoe.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        rollButton.setOnClickListener {
+            resultText.text = rollDice().toString()
+        }
+
+    }
+
+    private fun rollDice(): Int {
+        return Random().nextInt(6) + 1
     }
 }
