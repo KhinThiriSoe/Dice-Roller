a927a623b3d0722342d297e83346851c44969128
Khin <khinthirisoe.mdy@gmail.com>
2019-05-24 17:40:22 +0700

    variable declaration with lateinit

--- app/src/main/java/com/khinthirisoe/diceroller/MainActivity.kt
+++ app/src/main/java/com/khinthirisoe/diceroller/MainActivity.kt
@@ -8,32 +8,36 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val diceImage: ImageView = findViewById(R.id.dice_image)
 
         rollButton.setOnClickListener {
 
-            val drawableResource = when (rollDice()) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-
-            }
-
-            diceImage.setImageResource(drawableResource)
+            rollDice()
 
         }
 
     }
 
-    private fun rollDice(): Int {
-        return Random().nextInt(6) + 1
+    private fun rollDice(){
+
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
     }
 }
